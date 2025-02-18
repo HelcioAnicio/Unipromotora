027ea00b662ee99399f8f87bafdc5417b664acb2
Helcio Anicio <helcioanicio19@gmail.com>
2025-02-18 06:32:05 -0300

    Feat: First commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Header } from './components/header';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <h1 className='text-3xl font-bold text-red-500 underline'>
+        Hello world!
+      </h1>
+    </>
+  );
+}
+
+export default App;
