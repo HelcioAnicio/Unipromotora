d180bcba59f6c7af56eb1b2e851899a1280260e9
Helcio Anicio <helcioanicio19@gmail.com>
2025-02-25 08:22:30 -0300

    feat: Improve header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -36,6 +36,41 @@ export const Header = () => {
           </li>
         </ul>
       </nav>
+      <menu
+        type="toolbar"
+        className="absolute top-0 left-0 z-50 flex h-full w-full flex-col gap-10 bg-[#231D31] p-20"
+      >
+        Menu de navegação
+        <nav className="min-w-1/2 lg:block">
+          <ul className="flex flex-col justify-between gap-10 text-sm">
+            <li className="flex items-center">
+              <a href="#ourProducts" className="flex items-center gap-3">
+                Soluções <MdKeyboardArrowDown />
+              </a>
+            </li>
+            <li className="flex items-center">
+              <a href="#caroussel" className="flex items-center gap-3">
+                Parceiros <MdKeyboardArrowDown />
+              </a>
+            </li>
+            <li className="flex items-center">
+              <a href="#future" className="flex items-center gap-3">
+                Sobre nós <MdKeyboardArrowDown />
+              </a>
+            </li>
+            <li className="flex items-center">
+              <a href="#footer" className="flex items-center gap-3">
+                Contato <MdKeyboardArrowDown />
+              </a>
+            </li>
+            <li className="flex items-center gap-3">
+              <button className="rounded-2xl bg-[#03DB66] p-2">
+                <a href="#form">Simular crédito</a>
+              </button>
+            </li>
+          </ul>
+        </nav>
+      </menu>
     </header>
   );
 };
