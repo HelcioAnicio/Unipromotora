b43cb20cea20d9e3ffdf3e4c5f72cd34b0715dba
Helcio Anicio <helcioanicio19@gmail.com>
2025-02-26 07:21:27 -0300

    Feat: Implemented menu mobile

--- src/components/caroussel.tsx
+++ src/components/caroussel.tsx
@@ -5,55 +5,52 @@ import fitEconomy from "/fitEconomy.png";
 
 export const Caroussel = () => {
   return (
-    <section
-      className="group flex w-full items-center justify-start gap-16 overflow-hidden py-10"
-      id="caroussel"
-    >
-      <div className="group-hover:paused animate-scroll flex w-full justify-between gap-16">
+    <section className="group m-auto flex overflow-hidden py-10" id="caroussel">
+      <div className="group-hover:pausd animate-scroll flex">
         <img
           src={prataDigital}
           alt="logo Prata Digital"
-          className="data-text={text}> w-32"
+          className="data-text={text} mx-10 w-32 max-w-none"
         />
         <img
           src={santander}
           alt="logo Santander"
-          className="data-text={text}> w-32"
+          className="data-text={text} mx-10 w-32 max-w-none"
         />
         <img
           src={fitEconomy}
           alt="logo Fit Economy"
-          className="data-text={text}> w-24"
+          className="data-text={text} mx-10 w-24 max-w-none"
         />
         <img
           src={digital}
           alt="logo digital"
-          className="data-text={text}> w-24"
+          className="data-text={text} mx-10 w-24 max-w-none"
         />
       </div>
       <div
-        className="group-hover:paused animate-scroll flex w-full justify-between gap-16"
+        className="group-hover:paused animate-scroll flex"
         aria-hidden="true"
       >
         <img
           src={prataDigital}
           alt="logo Prata Digital"
-          className="data-text={text}> w-32"
+          className="data-text={text} mx-10 w-32 max-w-none"
         />
         <img
           src={santander}
           alt="logo Santander"
-          className="data-text={text}> w-32"
+          className="data-text={text} mx-10 w-32 max-w-none"
         />
         <img
           src={fitEconomy}
           alt="logo Fit Economy"
-          className="data-text={text}> w-24"
+          className="data-text={text} mx-10 w-24 max-w-none"
         />
         <img
           src={digital}
           alt="logo digital"
-          className="data-text={text}> w-24"
+          className="data-text={text} mx-10 w-24 max-w-none"
         />
       </div>
     </section>
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,76 +1,110 @@
 import { VscMenu } from "react-icons/vsc";
+import { IoMdClose } from "react-icons/io";
 import { MdKeyboardArrowDown } from "react-icons/md";
 import logo from "/header.png";
+import { useState } from "react";
 
 export const Header = () => {
+  const [menuActived, setMenuActived] = useState(false);
+
+  const menuMobile = () => {
+    setMenuActived(!menuActived);
+  };
+
   return (
-    <header className="flex w-full items-center justify-between bg-[#231D31] px-5 py-5 text-white">
+    <header className="relative flex w-full items-center justify-between bg-[#231D31] px-5 py-5 text-white">
       <img src={logo} alt="Logo and name of company" className="w-32" />
-      <VscMenu className="lg:hidden" />
+      <button className="cursor-pointer" onClick={menuMobile}>
+        {menuActived === false ? (
+          <VscMenu className="size-10 p-2 lg:hidden" />
+        ) : (
+          <IoMdClose className="size-10 p-2 lg:hidden" />
+        )}
+      </button>
+      {menuActived === true && (
+        <nav className="absolute top-full right-0 z-50 w-70 flex-col space-y-7 rounded-b-lg bg-[#231D31] px-6 py-10">
+          <img src={logo} alt="Logo and name of company" className="w-32" />
+          <menu
+            type="toolbar"
+            className="flex flex-col justify-between gap-7 text-sm"
+          >
+            <li className="flex items-center">
+              <a
+                href="#ourProducts"
+                className="flex items-center gap-3 px-5 py-2"
+                onClick={() => setMenuActived(!menuActived)}
+              >
+                Soluções <MdKeyboardArrowDown />
+              </a>
+            </li>
+            <li className="flex items-center">
+              <a
+                href="#caroussel"
+                className="flex items-center gap-3 px-5 py-2"
+                onClick={() => setMenuActived(!menuActived)}
+              >
+                Parceiros <MdKeyboardArrowDown />
+              </a>
+            </li>
+            <li className="flex items-center">
+              <a
+                href="#future"
+                className="flex items-center gap-3 px-5 py-2"
+                onClick={() => setMenuActived(!menuActived)}
+              >
+                Sobre nós <MdKeyboardArrowDown />
+              </a>
+            </li>
+            <li className="flex items-center">
+              <a
+                href="#footer"
+                className="flex items-center gap-3 px-5 py-2"
+                onClick={() => setMenuActived(!menuActived)}
+              >
+                Contato <MdKeyboardArrowDown />
+              </a>
+            </li>
+            <li className="flex items-center gap-3 px-5 py-2">
+              <button className="rounded-2xl bg-[#03DB66] p-2">
+                <a href="#form">Simular crédito</a>
+              </button>
+            </li>
+          </menu>
+        </nav>
+      )}
+
       <nav className="hidden min-w-1/2 lg:block">
         <ul className="flex justify-between gap-10 text-sm">
           <li className="flex items-center">
-            <a href="#ourProducts" className="flex items-center gap-3">
+            <a
+              href="#ourProducts"
+              className="flex items-center gap-3 px-5 py-2"
+            >
               Soluções <MdKeyboardArrowDown />
             </a>
           </li>
           <li className="flex items-center">
-            <a href="#caroussel" className="flex items-center gap-3">
+            <a href="#caroussel" className="flex items-center gap-3 px-5 py-2">
               Parceiros <MdKeyboardArrowDown />
             </a>
           </li>
           <li className="flex items-center">
-            <a href="#future" className="flex items-center gap-3">
+            <a href="#future" className="flex items-center gap-3 px-5 py-2">
               Sobre nós <MdKeyboardArrowDown />
             </a>
           </li>
           <li className="flex items-center">
-            <a href="#footer" className="flex items-center gap-3">
+            <a href="#footer" className="flex items-center gap-3 px-5 py-2">
               Contato <MdKeyboardArrowDown />
             </a>
           </li>
-          <li className="flex items-center gap-3">
+          <li className="flex items-center gap-3 px-5 py-2">
             <button className="rounded-2xl bg-[#03DB66] p-2">
               <a href="#form">Simular crédito</a>
             </button>
           </li>
         </ul>
       </nav>
-      <menu
-        type="toolbar"
-        className="absolute top-0 left-0 z-50 hidden h-full w-full flex-col gap-10 bg-[#231D31] p-20"
-      >
-        Menu de navegação
-        <nav className="min-w-1/2 lg:block">
-          <ul className="flex flex-col justify-between gap-10 text-sm">
-            <li className="flex items-center">
-              <a href="#ourProducts" className="flex items-center gap-3">
-                Soluções <MdKeyboardArrowDown />
-              </a>
-            </li>
-            <li className="flex items-center">
-              <a href="#caroussel" className="flex items-center gap-3">
-                Parceiros <MdKeyboardArrowDown />
-              </a>
-            </li>
-            <li className="flex items-center">
-              <a href="#future" className="flex items-center gap-3">
-                Sobre nós <MdKeyboardArrowDown />
-              </a>
-            </li>
-            <li className="flex items-center">
-              <a href="#footer" className="flex items-center gap-3">
-                Contato <MdKeyboardArrowDown />
-              </a>
-            </li>
-            <li className="flex items-center gap-3">
-              <button className="rounded-2xl bg-[#03DB66] p-2">
-                <a href="#form">Simular crédito</a>
-              </button>
-            </li>
-          </ul>
-        </nav>
-      </menu>
     </header>
   );
 };
