3b5840b8971eaa46c9f77fe3dd2178e10f3f8ba1
Helcio Anicio <helcioanicio19@gmail.com>
2025-03-03 07:15:20 -0300

    Fix: Improve animation scroll

--- src/components/caroussel.tsx
+++ src/components/caroussel.tsx
@@ -5,8 +5,8 @@ import fitEconomy from "/fitEconomy.png";
 
 export const Caroussel = () => {
   return (
-    <section className="group m-auto flex overflow-hidden py-10" id="caroussel">
-      <div className="group-hover:pausd animate-scroll flex">
+    <section className="group flex w-full overflow-hidden py-10" id="caroussel">
+      <div className="group-hover:paused animate-scroll flex flex-nowrap">
         <img
           src={prataDigital}
           alt="logo Prata Digital"
@@ -20,18 +20,13 @@ export const Caroussel = () => {
         <img
           src={fitEconomy}
           alt="logo Fit Economy"
-          className="data-text={text} mx-10 w-24 max-w-none"
+          className="data-text={text} mx-10 w-32 max-w-none"
         />
         <img
           src={digital}
           alt="logo digital"
-          className="data-text={text} mx-10 w-24 max-w-none"
+          className="data-text={text} mx-10 w-32 max-w-none"
         />
-      </div>
-      <div
-        className="group-hover:paused animate-scroll flex"
-        aria-hidden="true"
-      >
         <img
           src={prataDigital}
           alt="logo Prata Digital"
@@ -45,12 +40,12 @@ export const Caroussel = () => {
         <img
           src={fitEconomy}
           alt="logo Fit Economy"
-          className="data-text={text} mx-10 w-24 max-w-none"
+          className="data-text={text} mx-10 w-32 max-w-none"
         />
         <img
           src={digital}
           alt="logo digital"
-          className="data-text={text} mx-10 w-24 max-w-none"
+          className="data-text={text} mx-10 w-32 max-w-none"
         />
       </div>
     </section>
