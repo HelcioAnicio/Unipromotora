4524645e96eecda5e75d8a0a72095e5af9f8b68e
Helcio Anicio <helcioanicio19@gmail.com>
2025-03-25 06:34:25 -0300

    Feat: Improve SEO and acessibility

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -9,6 +9,7 @@ interface Form {
   cellphone: string;
   cpf: string;
   font: string;
+  service: string;
 }
 
 export const Form = () => {
@@ -18,6 +19,7 @@ export const Form = () => {
     cellphone: "",
     cpf: "",
     font: "",
+    service: "",
   });
 
   const [formItsOk, setFormItsOk] = useState("none");
@@ -72,9 +74,6 @@ export const Form = () => {
         },
       );
 
-      console.log("Response Data:", response.data);
-      console.log("Response Status:", response.status);
-
       if (response.status === 200) {
         setFormItsOk("ok");
         setTimeout(() => {
@@ -102,12 +101,22 @@ export const Form = () => {
   };
 
   return (
-    <section className="m-auto flex w-full max-w-5xl flex-col gap-14 pb-10 md:flex-row md:gap-10 md:px-5 md:pb-20">
+    <section
+      className="m-auto flex w-full max-w-5xl flex-col gap-14 pb-10 md:flex-row md:gap-10 md:px-5 md:pb-20"
+      aria-labelledby="form-title"
+    >
+      <h1 id="form-title" className="sr-only">
+        Formulário de Cadastro
+      </h1>
       <form
         className="relative flex w-full flex-col items-center py-10"
         id="form"
         onSubmit={registerLead}
+        aria-describedby="form-description"
       >
+        <p id="form-description" className="sr-only">
+          Preencha o formulário abaixo para enviar seus dados.
+        </p>
         <div className="flex w-full max-w-md flex-col items-start gap-5 rounded-2xl bg-[#03DB66] px-5 py-10 text-white">
           <div className="flex w-full flex-col">
             <label className="text-sm" htmlFor="name">
@@ -122,6 +131,7 @@ export const Form = () => {
               value={dataForm.fullName}
               placeholder="Seu nome completo"
               required
+              aria-required="true"
             />
           </div>
           <div className="flex w-full flex-col">
@@ -137,6 +147,7 @@ export const Form = () => {
               value={dataForm.email}
               placeholder="email@email.com"
               required
+              aria-required="true"
             />
           </div>
           <div className="flex w-full flex-col">
@@ -152,6 +163,7 @@ export const Form = () => {
               value={dataForm.cellphone}
               placeholder="(XX) XXXXX-XXXX"
               required
+              aria-required="true"
             />
           </div>
           <div className="flex w-full flex-col">
@@ -167,6 +179,7 @@ export const Form = () => {
               value={dataForm.cpf}
               placeholder="xxx.xxx.xxx-xx"
               required
+              aria-required="true"
             />
           </div>
           <div className="flex w-full flex-col">
@@ -179,7 +192,9 @@ export const Form = () => {
               value={dataForm.font}
               onChange={handleInputValues}
               className="rounded-2xl bg-white px-4 py-1 text-gray-400 outline-none"
+              aria-required="true"
             >
+              <option value="">Selecione sua fonte de renda</option>
               <option value="beneficiario">Beneficiário (INSS)</option>
               <option value="aposentado ou servidor publico">
                 Aposentado ou Servidor Público
@@ -188,28 +203,64 @@ export const Form = () => {
                 Trabalho com Carteira Assinada
               </option>
               <option value="recebendo auxilio">
-                Recebendo Auxilio (Bolsa familia)
+                Recebendo Auxílio (Bolsa família)
               </option>
               <option value="autonomo/pj">Autônomo/PJ</option>
               <option value="outra">Outra</option>
             </select>
           </div>
+
+          <div className="flex w-full flex-col">
+            <label className="text-sm" htmlFor="service">
+              Serviço desejado
+            </label>
+            <select
+              name="service"
+              id="service"
+              value={dataForm.service}
+              onChange={handleInputValues}
+              className="rounded-2xl bg-white px-4 py-1 text-gray-400 outline-none"
+              aria-required="true"
+            >
+              <option value="" disabled>
+                Selecione o serviço desejado
+              </option>
+              <option value="antecipação-saque-aniversário">
+                Antecipação do saque aniversário
+              </option>
+              <option value="debito-em-conta-inss">Débito em conta INSS</option>
+              <option value="empréstimo-bolsa-família">
+                Empréstimo bolsa família
+              </option>
+              <option value="quero-conhecer-todos">Quero conhecer todos</option>
+            </select>
+          </div>
+
           <button
             className="cursor-pointer rounded-3xl bg-[#237745] px-8 py-2 text-white hover:bg-[#3c6b50]"
             type="submit"
             onClick={registerLead}
+            aria-label="Enviar formulário para receber uma proposta"
           >
             Quero uma proposta
           </button>
         </div>
         {formItsOk === "ok" && (
-          <div className="absolute top-0 left-0 z-50 flex h-full w-dvw flex-col items-center justify-center gap-5 bg-white">
+          <div
+            className="absolute top-0 left-0 z-50 flex h-full w-dvw flex-col items-center justify-center gap-5 bg-white"
+            role="alert"
+            aria-live="assertive"
+          >
             <FaCheckCircle className="size-56 text-green-500" />
             <h2>Formulário enviado com sucesso</h2>
           </div>
         )}
         {formItsOk === "error" && (
-          <div className="absolute top-0 left-0 z-50 flex h-full w-dvw flex-col items-center justify-center gap-5 bg-white">
+          <div
+            className="absolute top-0 left-0 z-50 flex h-full w-dvw flex-col items-center justify-center gap-5 bg-white"
+            role="alert"
+            aria-live="assertive"
+          >
             <IoMdCloseCircle className="size-56 text-red-500" />
             <h2>Favor preencher todos os dados corretamente</h2>
           </div>
