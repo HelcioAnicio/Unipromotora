d40ee05d11b87dedc0015f81b451ee637f4a7d52
Helcio Anicio <helcioanicio19@gmail.com>
2025-03-25 06:37:53 -0300

    Fix: Fixed size of text, and text on wrong place

--- src/components/apresentation.tsx
+++ src/components/apresentation.tsx
@@ -8,13 +8,13 @@ export const Apresentation = () => {
     <section className="h-[calc(100dvh-80px)] w-full overflow-hidden rounded-br-[62px] bg-[#E7F6E9] xl:h-[calc(100dvh-94px)]">
       <div className="adapte m-auto flex h-full w-full max-w-6xl flex-col items-center justify-between gap-10 pt-10 lg:flex-row">
         <div className="flex flex-col items-center justify-center gap-7 pt-10 md:w-1/2 lg:items-start lg:pl-10">
-          <h1 className="max-w-60 text-center text-2xl md:max-w-96 md:text-5xl lg:text-left">
+          <h1 className="max-w-60 text-center text-2xl md:max-w-96 md:text-4xl lg:text-left">
             Transforme sua vida com o{" "}
             <span className="font-extrabold text-[#03db66]">
               crédito responsável
             </span>
           </h1>
-          <button className="rounded-3xl bg-[#237745] p-2 px-8 text-[#00FF75] hover:bg-[#324d3e] hover:outline-lime-950 active:opacity-85 md:text-3xl">
+          <button className="rounded-3xl bg-[#237745] p-2 px-8 text-[#00FF75] hover:bg-[#324d3e] hover:outline-lime-950 active:opacity-85 md:text-2xl">
             <a href="#form">Simular crédito</a>
           </button>
         </div>
